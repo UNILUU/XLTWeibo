8202be095ab0f235556089341bc0da24b36c54b7
xiaolu <iceeluu@gmail.com>
2018-12-02 19:24:01 -0600

    add file

---
+++ XLTWeibo/XLTWeibo/Classes/Profile/ProfileTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.swift
+//  XLTWeibo
+//
+//  Created by  on 12/2/18.
+//  Copyright © 2018 Xiaolu Tian. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITabViewController {
+
+}
